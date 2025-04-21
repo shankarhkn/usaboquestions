592ed6f24a27be215f7eaa56870143e1005ffe9d
Shankar Harikrishnan <shankarhkn@gmail.com>
2025-04-20 20:01:28 -0500

    Update index.js

--- usabo-api/index.js
+++ usabo-api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Enable CORS to allow frontend to make requests
 app.use(cors());
