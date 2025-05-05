b83f0b936897f26f6e2659f1575e5d1d0a61dfe9
Shankar Harikrishnan <shankarhkn@gmail.com>
2025-05-05 10:55:23 -0500

    Update index.js

--- usabo-api/index.js
+++ usabo-api/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 // Load questions from JSON file
-const questionsPath = path.join(__dirname, 'usabo-api/questions.json');
+const questionsPath = path.join(__dirname, 'questions.json');
 let questions = [];
 
 try {
